6c10816db62db0b2eb61b9ae156866bfda6aa137
Xavier Remacle <Xavier_Remacle_@hotmail.com>
2019-11-10 13:13:50 -0500

    Completed Exercise 4

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # udemy_asp.net_mvc
+##### Created by Mosh Hamedani
+> [Try the course out][udemy_course]  
+> This content is not mine and I do not claim it to be my own.  
+
+The purpose of this repo is to practice git and to have an active link for future reference via my commits  
+
+Last commit: Completed Exercise 3| Progress: 31%  
+
+## Vidly
+A simple ASP.net MVC web app for a 'Blockbuster' style rental service.  
+
+## Build
+Vidly.sln  
+
+[udemy_course]: https://www.udemy.com/course/the-complete-aspnet-mvc-5-course/
--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -3,6 +3,8 @@ using System.Linq;
 using System.Web.Mvc;
 using Vidly.Models;
 using System.Data.Entity;
+using Vidly.ViewModels;
+
 namespace Vidly.Controllers
 {
     public class CustomersController : Controller
@@ -17,6 +19,36 @@ namespace Vidly.Controllers
         protected override void Dispose(bool disposing)
         {
             _context.Dispose();
+        }
+
+
+        public ActionResult New()
+        {
+            var membershipTypes = _context.MembershipTypes.ToList();
+
+            var viewModel = new CustomerFormViewModel
+            {
+                MembershipTypes = membershipTypes
+            };
+            return View("CustomerForm",viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Customer customer)
+        {
+            if (customer.Id == 0)
+                _context.Customers.Add(customer);
+            else
+            {
+                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+                customerInDb.Name = customer.Name;
+                customerInDb.Birthdate = customer.Birthdate;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+                customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+
+            }
+            _context.SaveChanges();
+            return RedirectToAction("Index","Customers");
         }
 
         public ViewResult Index()
@@ -35,6 +67,21 @@ namespace Vidly.Controllers
                 return HttpNotFound();
 
             return View(customer);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+                return HttpNotFound();
+
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+
+            return View("CustomerForm",viewModel);
         }
     }
 }
